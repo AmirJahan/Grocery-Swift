62638cb382105f93f9478a2e0e2f859e99c2a473
Amir J <amir.jahan@gmail.com>
2017-08-20 17:04:26 -0700

    first commit

---
+++ Swift Groceries/AppData.swift
@@ -0,0 +1,13 @@
+//
+//  AppData.swift
+//  Swift Groceries
+//
+//  Created by Amir on 7/30/17.
+//  Copyright © 2017 Amir. All rights reserved.
+//
+
+import UIKit
+
+class AppData: NSObject {
+
+}
